6b5dcd1c30c38733d1c0950883476a1d150c5dda
Gutierrez, Andrew <atgutier@butler.edu>
2019-09-03 19:42:26 -0400

    gumbo_again

--- R Scripts/Gumbos_clean_2019.R
+++ R Scripts/Gumbos_clean_2019.R
@@ -24,7 +24,29 @@ library_2019[[set]]<- tibble(sequence= library_2019[[set]],
                              set= set)
 
 
+#make library amplication sequence 
+amplificationseq1<-'CCCGCACCAGGTGCCGAATGGCGCGCCA'
+amplificationseq2<-'GCGATCCTAGGGCGATCAGGC'
+stopcodons<-'TAAGTAGCTGA'
 
+#this is the random WD sequences at length 20 
+rbinom(20, 1, 0.5)
+hold<-list()
+for ( i in 1:50){
+  hold[[i]]<-rbinom(20, 1, 0.5)
+}
+
+
+
+Whold<-list()
+for (i in 1:length(hold)){
+  Whold[[i]]<-gsub('0', 'W', hold[[i]])
+}
+
+WDhold<-list()
+for (i in 1:length(Whold)) {
+  WDhold[[i]]<-gsub('1', 'D', Whold[[i]])
+}
 
 
 
