80a08260b8d7f784a250a8093c1ea033eb8a22f6
Gutierrez, Andrew <atgutier@butler.edu>
2019-09-23 10:46:21 -0400

    Update AAtoNTconverter.R

--- AAtoNTconverter.R
+++ AAtoNTconverter.R
@@ -12,11 +12,26 @@ Codon_table<-tibble(aa= c('F','S','Y','C','L','P','H','R','I','T','N','V','A','D
 hold_a<-strsplit(library_2019$combinatorial$sequence,'')
 
 
+
+#the following for loop didnt work 
 NThold<-list()
 
 for( i in 1:length(Codon_table$aa)){
-  NThold[[i]]<-gsub(Codon_table$aa, Codon_table$codon_1, hold_a)
+  NThold[[i]]<-gsub(Codon_table$aa, Codon_table$codon_1, hold_a[[i]])
 }
+
+
+#this didnt work either, need to find a better way 
+gsub(Codon_table$aa, Codon_table$codon_1, library_2019$combinatorial$sequence[[1]])
+
+
+
+#str_replace works.. but need to work on it 
+##this could actually work if its manually typed out bc we only use amino acids for the most part 
+###would be very jank but it would work lol 
+Ghold<-str_replace_all(library_2019$combinatorial$sequence, "G", 'GGT')
+GWhold<-str_replace_all(Ghold, "W", 'TGG')
+GWDhold<-str_replace_all(GWhold, "D", 'GAT')
 
 
 
