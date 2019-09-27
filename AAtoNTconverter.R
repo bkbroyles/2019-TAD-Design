f115f6f07162a2e7b2b7535e506a35b38adc6832
Gutierrez, Andrew <atgutier@butler.edu>
2019-09-27 16:24:19 -0400

    AA to NT converter funciton

--- AAtoNTconverter.R
+++ AAtoNTconverter.R
@@ -5,33 +5,61 @@ tibble(aa=c("W", 'Y', 'F'),
        codon_2=c('aa','bb','cc'))
 
 
-Codon_table<-tibble(aa= c('F','S','Y','C','L','P','H','R','I','T','N','V','A','D','G','E','K','M','Q'),
-                    codon_1=c('TTT','TCT','TAT','TGT','CTT','CCT','CAT','CGT','ATT','ACT','AAT','GTT','GCT','GAT','GGT','GAA','AAA','ATG','CAA'),
-                    codon_2= c('TTC','TCC','TAC','TGC','CTC','CCC','CAC','CGC','ATC','ACC','AAC','GTC','GCC','GAC','GGC','GAG','AAG','ATG','CAG'))
-
-hold_a<-strsplit(library_2019$combinatorial$sequence,'')
+Codon_table<-tibble(aa= c('F','S','Y','C','L','P','H','R','I','T','N','V','A','D','G','E','K','M','Q', 'W'),
+                    codon_1=c('TTT','TCT','TAT','TGT','CTT','CCT','CAT','CGT','ATT','ACT','AAT','GTT','GCT','GAT','GGT','GAA','AAA','ATG','CAA', 'TGG'),
+                    codon_2= c('TTC','TCC','TAC','TGC','CTC','CCC','CAC','CGC','ATC','ACC','AAC','GTC','GCC','GAC','GGC','GAG','AAG','ATG','CAG', 'TGG'))
 
 
+#making a funciton so that we dont have to keep retyping the 
+# CodontoNuc(WHatever$sequence)
 
-#the following for loop didnt work 
-NThold<-list()
+CodontoNuc<-function(df){
+holdthetibble<-tibble(aa_seq= df, nuc_seq = '')
 
-for( i in 1:length(Codon_table$aa)){
-  NThold[[i]]<-gsub(Codon_table$aa, Codon_table$codon_1, hold_a[[i]])
+
+
+for (i in 1:nrow(holdthetibble)) {
+  #make a vector for each aa sequence
+  seq <- strsplit(holdthetibble$aa_seq[i], '')[[1]]
+  
+  #just initialize a new nucleotide sequence for each aa sequence
+  new_seq <- ''
+  
+  for(j in 1:length(seq)){
+    #match aa letter with codon, and return as a character vector
+    new_codon <- Codon_table %>%
+      filter(aa == seq[j]) %>%
+      select(codon_1) %>% 
+      as.character()
+    #pastes all the codons as one nucleotide sequence
+    new_seq <- paste(new_seq, new_codon, sep = '')
+  }
+#save nucleotide sequence
+holdthetibble$nuc_seq[i] <- new_seq
+}
+return(holdthetibble)
 }
 
+library_2019$
 
-#this didnt work either, need to find a better way 
-gsub(Codon_table$aa, Codon_table$codon_1, library_2019$combinatorial$sequence[[1]])
+library_2019_sequences<-list()
 
+library_2019_sequences$W_first<-CodontoNuc(library_2019$W_first$sequence)
 
+library_2019_sequences$combinatorial<- CodontoNuc(library_2019$combinatorial$sequence)
 
-#str_replace works.. but need to work on it 
-##this could actually work if its manually typed out bc we only use amino acids for the most part 
-###would be very jank but it would work lol 
-Ghold<-str_replace_all(library_2019$combinatorial$sequence, "G", 'GGT')
-GWhold<-str_replace_all(Ghold, "W", 'TGG')
-GWDhold<-str_replace_all(GWhold, "D", 'GAT')
+library_2019_sequences$D_first<-CodontoNuc(library_2019$D_first$sequence)
+
+library_2019_sequences$set_g_spacing<- CodontoNuc(library_2019$set_g_spacing$sequence)
+
+library_2019_sequences$g_in<-CodontoNuc(library_2019$g_in$sequence)
+
+library_2019_sequences$g_out<-CodontoNuc(library_2019$g_out$sequence)
+
+library_2019_sequences$terminal_wd<-CodontoNuc(library_2019$temrinal_wd$sequence)
+
+library_2019_sequences$internal_wd<-CodontoNuc(library_2019$internal_wd$sequence)
+
 
 
 
